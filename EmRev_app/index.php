fc0afeb7fe618419c36061845799c696a46ed159
yh72167 <yh.19972372@gmail.com>
2020-11-19 19:47:11 +0900

    [add]ホーム画面のhtml配置・scss用の変数・関数一部設定

--- EmRev_app/index.php
+++ EmRev_app/index.php
@@ -2,10 +2,217 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>Project</title></title>
+  <title>Project</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
   <link rel="stylesheet" href="root/css/style.css">
 </head>
 <body>
+  <!-- ヘッダー関係 -->
+  <header class="header">
+    <!-- タイトル -->
+    <h1 class="header_title">EmRev</h1>
+    <!-- ナビゲーション(セッション内容で切り替える) -->
+    <nav>
+      <ui>
+        <li>LOGIN</li>
+        <li>SIGNUP</li>
+      </ui>
+    </nav>
+  </header>
 
+  <!-- ヒーローバナー -->
+  <session class="hero">
+    <!-- テキスト関係 -->
+      <div class="hero__text-content">
+        <span class="hero__text-catchTheam">
+          キャッチテーマ
+        </span>
+        <div class="hero__text-about">
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+        </div>
+        <div class="hero__text-aboutStyle">
+          <a class="hero__text-aboutLink">このアプリについて</a>
+        </div>
+      </div>
+  </session>
+
+  <!-- 新着レビューコンテンツ -->
+  <session class="review">
+
+    <!-- レビューコンテンツ(今は仮レイアウトの都合上複数同じ要素を作っているがphpを書き始めた際にはfor文で回す) -->
+    <div class="review__content review__contentPosition-1">
+
+      <!-- イメージ画像 -->
+      <img class="review__image" rel="#">
+
+      <!-- ユーザー名 -->
+      <div href="#" class="review__userName-style">
+        <span class="review__userName">
+          ユーザー名
+        </span>
+      </div>
+
+      <!-- 会社名 -->
+      <div href="#" class="review__companyName-style">
+        <span class="review__companyName">会社名</span>
+      </div>
+
+      <!-- 会社の業界 -->
+      <div href="#" class="review__industry-style">
+        <span class="review__industry">会社の業界</span>
+      </div>
+
+      <!-- レビュー内容(総評) -->
+      <div class="review__generalComment-style">
+        <div class="review__generalComment">
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+        </div>
+      </div>
+
+      <!-- 詳細を見る -->
+      <div href="#" class="review__detail-style">
+        <span class="review__detail">
+        詳細を見る
+        </span>
+      </div>
+    </div>
+
+    <!-- レビューコンテンツ(今は仮レイアウトの都合上複数同じ要素を作っているがphpを書き始めた際にはfor文で回す) -->
+    <div class="review__content review__contentPosition-2">
+
+      <!-- イメージ画像 -->
+      <img class="review__image" rel="#">
+
+      <!-- ユーザー名 -->
+      <div href="#" class="review__username-style">
+        <span class="review__userName">
+          ユーザー名
+        </span>
+      </div>
+
+      <!-- 会社名 -->
+      <div href="#" class="review__companyName-style">
+        <span class="review__companyName">会社名</span>
+      </div>
+
+      <!-- 会社の業界 -->
+      <div href="#" class="review__industry-style">
+        <span class="review__industry">会社の業界</span>
+      </div>
+
+      <!-- レビュー内容(総評) -->
+      <div class="review__generalComment-style">
+        <div class="review__generalComment">
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+        </div>
+      </div>
+
+      <!-- 詳細を見る -->
+      <div href="#" class="review__detail-style">
+        <span class="review__detail">
+        詳細を見る
+        </span>
+      </div>
+    </div>
+
+    <!-- レビューコンテンツ(今は仮レイアウトの都合上複数同じ要素を作っているがphpを書き始めた際にはfor文で回す) -->
+    <div class="review__content review__contentPosition-3">
+
+      <!-- イメージ画像 -->
+      <img class="review__image" rel="#">
+
+      <!-- ユーザー名 -->
+      <div href="#" class="review__userName-style">
+        <span class="review__userName">
+          ユーザー名
+        </span>
+      </div>
+
+      <!-- 会社名 -->
+      <div href="#" class="review__companyName-style">
+        <span class="review__companyName">会社名</span>
+      </div>
+
+      <!-- 会社の業界 -->
+      <div href="#" class="review__industry-style">
+        <span class="review__industry">会社の業界</span>
+      </div>
+
+      <!-- レビュー内容(総評) -->
+      <div class="review__generalComment-style">
+        <div class="review__generalComment">
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+          サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+        </div>
+      </div>
+
+      <!-- 詳細を見る -->
+      <div href="#" class="review__detail-style">
+        <span class="review__detail">
+        詳細を見る
+        </span>
+      </div>
+    </div>
+
+  </session>
+
+
+  <!-- このアプリについて -->
+  <session class="about">
+    <div class="about__content-style">
+
+      <div class="about__title">
+      このアプリについて
+      </div>
+
+      <div class="about__text">
+        サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+        サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+        サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+        サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+      </div>
+
+      <div class="about__link-style">
+
+      <!-- リンク先はセッション内のログイン情報に沿ってif文で変更する。ログインしている場合はマイページへ。していない場合はサインアップ画面へ移動する。-->
+        <div href="#" class="about__link">
+          このアプリを使ってみる
+        </div>
+      </div>
+    </div>
+  </session>
+
+    <!-- お問い合わせフォーム -->
+  <session class="contact">
+    <div class="contact__usernName">
+      お問い合わせ
+    </div>
+    <div class="contact__type-style">
+      <div class="contact__type">
+        お問い合わせの種類
+      </div>
+      <option>
+      </option>
+    </div>
+    <div class="contact__about">
+      <textarea name="" id="" cols="30" rows="10">
+      </textarea>
+    </div>
+    <div>送信</div>
+  </session>
+
+  <!-- フッター -->
+  <footer class="footer">
+  Copyright © yuitoHigashihara. All Rights Reserved
+  </footer>
 </body>
 </html>
