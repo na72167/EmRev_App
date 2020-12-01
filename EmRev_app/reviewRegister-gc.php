0fa957fa848b7ec896fae3eae7e12baf0f1861ce
yh72167 <yh.19972372@gmail.com>
2020-12-01 14:26:50 +0900

    [add]レビュー登録画面(総評)のコーディング　完成

--- EmRev_app/reviewRegister-gc.php
+++ EmRev_app/reviewRegister-gc.php
@@ -1,57 +1,54 @@
+<!-- GcはgeneralCommentの略 -->
 <?php
   // タイトルの読み込み
   $Page_Title = 'レビュー登録画面';
+  $Intro__Text_Title ='Review Register';
+  $Intro__Text_Sub ='総評';
+  $Intro__Img = 'src="src/img/docs.png"';
   // ヘッドの読み込み
   require('./head.php');
 ?>
 <body>
+
   <!--ヘッダー読み込み-->
   <?php
     require('./header.php');
-    ?>
-  <section>
-    <div>
-      <div>
+  ?>
 
-      </div>
-      <div>
+  <!-- ページ紹介要素ファイルの読み込み -->
+  <?php
+    require('./intro.php');
+  ?>
 
-      </div>
-    </div>
-    <img>
-  </section>
+<section class="revRegistGc-content">
 
-  <section>
-    <div>
-      <div></div>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
+    <div class="revRegistGc-content__wrap">
+      <?php
+      require('./revProgress.php');
+      ?>
+      <div class="revRegistGc-content__form-element">
+        <div class="revRegistGc-content__form-wrap">
+          <h4 class="revRegistGc-content__title">Post Company Review</h4>
+          <h1 class="revRegistGc-content__sub">総評</h1>
+          <from action="" class="revRegistGc-content__wrap">
 
-    <div>
+            <div class="revRegistGc-content__form-conciseTitle">総合的なこの会社の印象や評価を20文字以内でお願いします。</div>
+            <textarea class="revRegistGc-content__form-conciseAreaForm" placeholder="総評(簡潔にお願いします)"></textarea>
 
-      <div>
-        <input>
-        <input>
-        <input>
-        <input>
-        <input>
-        <input>
-        <input>
-        <input>
+            <div class="revRegistGc-content__form-title">総評</div>
+            <textarea class="revRegistGc-content__form-areaForm" placeholder="総評(詳しくお願いします)"></textarea>
+
+            <div class="revRegistGc-content__bottom-wrap">
+              <a href="reviewRegister-jr.php" class="revRegistGc-content__bottom-link"><bottom class="revRegistGc-content__bottom-return" href="reviewRegister-cc.php">前の項目へ</bottom></a>
+              <a href="reviewRegister-jw.php" class="revRegistGc-content__bottom-link"><bottom class="revRegistGc-content__bottom-next" href="reviewRegister-pc.php">次の項目へ</bottom></a>
+            </div>
+
+          </from>
+        </div>
       </div>
 
-      <div>
-        <bottom></bottom>
-        <bottom></bottom>
-      </div>
     </div>
   </section>
-
   <?php
   // フッター要素の読み込み
     require('./footer.php');
